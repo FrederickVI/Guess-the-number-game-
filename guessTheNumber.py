c0a5064abad9fe90a36527c4412d32f59ad651fb
Fred <77522346+FrederickVI@users.noreply.github.com>
2021-12-05 08:48:17 +0800

    Add files via upload

---
+++ guessTheNumber.py
@@ -0,0 +1,33 @@
+#This is the guess the number game.
+import random 
+import sys
+secretNumber = (random.randint(1,20))
+print("I am thinking of a number between 1 and 20")
+
+while True:                                      #Option to play the game
+    print("Do you want to play the game? yes/no")
+    response = input()
+    if response == "yes":
+        print("")
+        break
+    elif response == "no":
+        print("No one wants to play with me(")
+        sys.exit()
+    else:
+        print("Answer Must Be 'Yes' or 'No'")
+
+for guessesTaken in range(1,7):
+    print("Take a guess")
+    guess = int(input())
+
+    if guess < secretNumber:
+        print("Your guess is too low")
+    elif guess > secretNumber:
+        print("Your guess is too high")
+    else: 
+        break #This condition is the correct guess!
+
+if guess == secretNumber:
+    print("Good job! You guessed the number in " + str(guessesTaken) + " guesses")
+else:
+    print("You failed. The number was" + str(secretNumber))
